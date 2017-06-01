b6f531264464fd968ea01bea959124f565e1eed7
Nitishkumar Singh <nitishkumarsingh71@gmail.com>
2017-06-02 01:42:33 +0530

    Updated to WEB API

---
+++ AngularWebpackStarter/Controllers/MyController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AngularWebpackStarter.Controllers
+{
+    [RoutePrefix("api/my")]
+    public class MyController : ApiController
+    {
+        [Route("")]
+        [HttpGet]
+        public String Get()
+        {
+            return "Hello";
+        }
+    }
+}
